860e0ea9c20b491171e448a9e9ca55340ccb269e
MattScarthSaunders <m.scarsaund@gmail.com>
2024-09-20 17:46:38 +0100

    feat: domain and testing

---
+++ src/main/global/SqlDomainImpl.ts
@@ -0,0 +1,54 @@
+import { SqlDomain } from '@nodescript/adapter-sql-protocol';
+import { dep } from 'mesh-ioc';
+
+import { SqlDefinitionResult } from '../../../protocol/src/schema/SqlDefinitionResult.js';
+import { SqlModificationResult } from '../../../protocol/src/schema/SqlModificationResult.js';
+import { SqlQueryResult } from '../../../protocol/src/schema/SqlQueryResult.js';
+import { ConnectionManager } from './ConnectionManager.js';
+
+export class SqlDomainImpl implements SqlDomain {
+
+    @dep() private connectionManager!: ConnectionManager;
+
+    async connect(req: {
+        connectionUrl: string;
+    }): Promise<{}> {
+        await this.getConnection(req.connectionUrl);
+        return {};
+    }
+
+    async executeDefinition(req: {
+        connectionUrl: string;
+        definition: string;
+    }): Promise<{result: SqlDefinitionResult}> {
+        const connection = await this.getConnection(req.connectionUrl);
+        const result = await connection.define(req.definition);
+        return { result };
+    }
+
+    async executeModification(req: {
+        connectionUrl: string;
+        modification: string;
+        params: any[];
+    }): Promise<{result: SqlModificationResult}> {
+        const connection = await this.getConnection(req.connectionUrl);
+        const result = await connection.modify(req.modification, req.params);
+        return { result };
+    }
+
+    async executeQuery(req: {
+        connectionUrl: string;
+        query: string;
+        params: any[];
+    }): Promise<{result: SqlQueryResult}> {
+        const connection = await this.getConnection(req.connectionUrl);
+        const result = await connection.query(req.query, req.params);
+        return { result };
+    }
+
+    private async getConnection(databaseUrl: string) {
+        const conn = this.connectionManager.getConnection(databaseUrl);
+        await conn.connect();
+        return conn;
+    }
+}
---
+++ src/test/domain/SqlDomain.test.ts
@@ -0,0 +1,78 @@
+import assert from 'assert';
+
+import { runtime } from '../runtime.js';
+
+describe('SqlDomain', () => {
+
+    context('psql', () => {
+        beforeEach(async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            await runtime.Sql.executeDefinition({ connectionUrl,
+                definition: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
+        });
+        afterEach(async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            await runtime.Sql.executeDefinition({ connectionUrl,
+                definition: 'DROP TABLE test;' });
+        });
+
+        it('defines', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            const { result } = await runtime.Sql.executeDefinition({ connectionUrl,
+                definition: 'CREATE TABLE defined (id SERIAL PRIMARY KEY);' });
+            assert.ok(result.command);
+        });
+
+        it('modifies', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            const { result } = await runtime.Sql.executeModification({
+                connectionUrl,
+                modification: `INSERT INTO test (username) VALUES ($1);`,
+                params: ['test2']
+            });
+            assert.ok(result);
+        });
+
+        it('queries', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            const { result } = await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
+            assert.ok(result);
+        });
+    });
+    context('mysql', () => {
+        beforeEach(async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            await runtime.Sql.executeDefinition({ connectionUrl,
+                definition: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
+        });
+        afterEach(async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            await runtime.Sql.executeDefinition({ connectionUrl,
+                definition: 'DROP TABLE test;' });
+        });
+
+        it('defines', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            const { result } = await runtime.Sql.executeDefinition({ connectionUrl,
+                definition: 'CREATE TABLE defined (id SERIAL PRIMARY KEY);' });
+            assert.ok(result.command);
+        });
+
+        it('modifies', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            const { result } = await runtime.Sql.executeModification({
+                connectionUrl,
+                modification: `INSERT INTO test (username) VALUES (?);`,
+                params: ['test2']
+            });
+            assert.ok(result);
+        });
+
+        it('queries', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            const { result } = await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
+            assert.ok(result);
+        });
+    });
+
+});
