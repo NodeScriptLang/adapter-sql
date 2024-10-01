1bbd909ed6f8ec428cf995f9cc2a31249578c0d8
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2024-10-01 09:43:36 +0100

    test: connections pools (#3)
    * feat: get counters for pools and connections
    
    * test: mysql + psql connection management
    
    * test: pool lifecycle
    
    * fix: eol
    
    * test: increase timeout times
    
    * test: make method public, add expiry parameter

---
+++ src/test/connections/ConnectionManager.test.ts
@@ -0,0 +1,65 @@
+import assert from 'assert';
+
+import { runtime } from '../runtime.js';
+
+
+describe('Connection Manager', () => {
+
+    beforeEach(async () => {
+        await runtime.app.connectionManager.stop();
+        await runtime.app.connectionManager.start();
+    });
+
+    it('same url = single pool', async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+
+        runtime.app.connectionManager.getPool(connectionUrl);
+        runtime.app.connectionManager.getPool(connectionUrl);
+        const poolCount = runtime.app.connectionManager.poolCount;
+        assert.equal(poolCount, 1);
+    });
+
+    it('multiple urls = multiple pools', async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+        const connectionUrl2 = runtime.testMySqlUrl;
+
+        runtime.app.connectionManager.getPool(connectionUrl);
+        runtime.app.connectionManager.getPool(connectionUrl2);
+        const poolCount = runtime.app.connectionManager.poolCount;
+        assert.equal(poolCount, 2);
+    });
+
+    context('Postgres', () => {
+        it('closes + can reopen pools after TTL expires', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+
+            runtime.app.connectionManager.getPool(connectionUrl);
+            await new Promise(resolve => setTimeout(resolve, 1)); // pool age > expiry time
+            await runtime.app.connectionManager.closeExpired(0);
+
+            const sweptCount = runtime.app.connectionManager.poolCount;
+            assert.equal(sweptCount, 0, 'Expected no pools to be open');
+
+            runtime.app.connectionManager.getPool(connectionUrl);
+            const activeCount = runtime.app.connectionManager.poolCount;
+            assert.equal(activeCount, 1, 'Expected 1 pool to be open');
+        });
+    });
+
+    context('MySql', () => {
+        it('closes + can reopen pools after TTL expires', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+
+            runtime.app.connectionManager.getPool(connectionUrl);
+            await new Promise(resolve => setTimeout(resolve, 1)); // pool age > expiry time
+            await runtime.app.connectionManager.closeExpired(0);
+
+            const sweptCount = runtime.app.connectionManager.poolCount;
+            assert.equal(sweptCount, 0, 'Expected no pools to be open');
+
+            runtime.app.connectionManager.getPool(connectionUrl);
+            const activeCount = runtime.app.connectionManager.poolCount;
+            assert.equal(activeCount, 1, 'Expected 1 pool to be open');
+        });
+    });
+});
---
+++ src/test/connections/Postgres.test.ts
@@ -0,0 +1,64 @@
+import assert from 'assert';
+
+import { runtime } from '../runtime.js';
+
+
+describe('Postgres Connections', () => {
+    beforeEach(async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+        await runtime.Sql.executeDefinition({ connectionUrl,
+            definition: 'DROP TABLE IF EXISTS test;' });
+        await runtime.Sql.executeDefinition({ connectionUrl,
+            definition: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
+    });
+    afterEach(async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+        await runtime.Sql.executeDefinition({ connectionUrl,
+            definition: 'DROP TABLE test;' });
+    });
+
+    it('releases connection after use', async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+        await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
+        const pool = runtime.app.connectionManager.getPool(connectionUrl);
+        assert.deepEqual(pool.connectionCount, 0);
+    });
+
+    it('releases connection on error', async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+        try {
+            await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM nonexistent;', params: [] });
+        } catch (err) {
+            // ignore the error
+        }
+        const pool = runtime.app.connectionManager.getPool(connectionUrl);
+        assert.deepEqual(pool.connectionCount, 0);
+
+        const { result } = await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
+        assert.ok(result);
+    });
+
+    it('queue + release - pool size limit 10', async () => {
+        const connectionUrl = runtime.testPostgresUrl;
+        const concurrency = 15;
+        const runDurationMs = 30;
+
+        const queryPromises = [];
+        for (let i = 0; i < concurrency; i++) {
+            queryPromises.push(runtime.Sql.executeQuery({
+                connectionUrl,
+                query: `SELECT pg_sleep_for('${runDurationMs} milliseconds'::interval);`,
+                params: []
+            }));
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 20).unref()); // allow queries to start running
+
+        const pool = runtime.app.connectionManager.getPool(connectionUrl);
+        assert.equal(pool.connectionCount, 10);
+
+        await new Promise(resolve => setTimeout(resolve, runDurationMs).unref()); // allow initial batch to finish
+        assert.equal(pool.connectionCount, 5);
+        await Promise.all(queryPromises);
+    });
+});
