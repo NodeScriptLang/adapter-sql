64526067b051f139f551e5476f7ffba1c8cb529d
MattScarthSaunders <m.scarsaund@gmail.com>
2024-10-02 15:08:34 +0100

    test: use new endpoint

--- src/test/domain/SqlDomain.test.ts
+++ src/test/domain/SqlDomain.test.ts
@@ -1,83 +1,83 @@
-// import assert from 'assert';
+import assert from 'assert';
 
-// import { runtime } from '../runtime.js';
+import { runtime } from '../runtime.js';
 
-// describe('SqlDomain', () => {
+describe('SqlDomain', () => {
 
-//     context('psql', () => {
-//         beforeEach(async () => {
-//             const connectionUrl = runtime.testPostgresUrl;
-//             await runtime.Sql.executeDefinition({ connectionUrl,
-//                 definition: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
-//         });
-//         afterEach(async () => {
-//             const connectionUrl = runtime.testPostgresUrl;
-//             await runtime.Sql.executeDefinition({ connectionUrl,
-//                 definition: 'DROP TABLE test;' });
-//         });
+    context('psql', () => {
+        beforeEach(async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            await runtime.Sql.query({ connectionUrl,
+                query: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
+        });
+        afterEach(async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            await runtime.Sql.query({ connectionUrl,
+                query: 'DROP TABLE test;' });
+        });
 
-//         it('defines', async () => {
-//             const connectionUrl = runtime.testPostgresUrl;
-//             const result = await runtime.Sql.executeDefinition({
-//                 connectionUrl,
-//                 definition: 'CREATE TABLE psqldefined (id SERIAL PRIMARY KEY);'
-//             });
-//             assert.ok(result);
-//         });
+        it('defines', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            const result = await runtime.Sql.query({
+                connectionUrl,
+                query: 'CREATE TABLE psqldefined (id SERIAL PRIMARY KEY);'
+            });
+            assert.ok(result);
+        });
 
-//         it('modifies', async () => {
-//             const connectionUrl = runtime.testPostgresUrl;
-//             const { result } = await runtime.Sql.executeModification({
-//                 connectionUrl,
-//                 modification: `INSERT INTO test (username) VALUES ($1);`,
-//                 params: ['test2']
-//             });
-//             assert.ok(result);
-//         });
+        it('modifies', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            const { result } = await runtime.Sql.query({
+                connectionUrl,
+                query: `INSERT INTO test (username) VALUES ($1);`,
+                params: ['test2']
+            });
+            assert.ok(result);
+        });
 
-//         it('queries', async () => {
-//             const connectionUrl = runtime.testPostgresUrl;
-//             const { result } = await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
-//             assert.ok(result);
-//         });
-//     });
+        it('queries', async () => {
+            const connectionUrl = runtime.testPostgresUrl;
+            const { result } = await runtime.Sql.query({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
+            assert.ok(result);
+        });
+    });
 
-//     context('mysql', () => {
-//         beforeEach(async () => {
-//             const connectionUrl = runtime.testMySqlUrl;
-//             await runtime.Sql.executeDefinition({ connectionUrl,
-//                 definition: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
-//         });
-//         afterEach(async () => {
-//             const connectionUrl = runtime.testMySqlUrl;
-//             await runtime.Sql.executeDefinition({ connectionUrl,
-//                 definition: 'DROP TABLE test;' });
-//         });
+    context('mysql', () => {
+        beforeEach(async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            await runtime.Sql.query({ connectionUrl,
+                query: 'CREATE TABLE test (id SERIAL PRIMARY KEY, username VARCHAR(50));' });
+        });
+        afterEach(async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            await runtime.Sql.query({ connectionUrl,
+                query: 'DROP TABLE test;' });
+        });
 
-//         it('defines', async () => {
-//             const connectionUrl = runtime.testMySqlUrl;
-//             const result = await runtime.Sql.executeDefinition({
-//                 connectionUrl,
-//                 definition: 'CREATE TABLE mysqldefined (id SERIAL PRIMARY KEY);'
-//             });
-//             assert.ok(result);
-//         });
+        it('defines', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            const result = await runtime.Sql.query({
+                connectionUrl,
+                query: 'CREATE TABLE mysqldefined (id SERIAL PRIMARY KEY);'
+            });
+            assert.ok(result);
+        });
 
-//         it('modifies', async () => {
-//             const connectionUrl = runtime.testMySqlUrl;
-//             const { result } = await runtime.Sql.executeModification({
-//                 connectionUrl,
-//                 modification: `INSERT INTO test (username) VALUES (?);`,
-//                 params: ['test2']
-//             });
-//             assert.ok(result);
-//         });
+        it('modifies', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            const { result } = await runtime.Sql.query({
+                connectionUrl,
+                query: `INSERT INTO test (username) VALUES (?);`,
+                params: ['test2']
+            });
+            assert.ok(result);
+        });
 
-//         it('queries', async () => {
-//             const connectionUrl = runtime.testMySqlUrl;
-//             const { result } = await runtime.Sql.executeQuery({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
-//             assert.ok(result);
-//         });
-//     });
+        it('queries', async () => {
+            const connectionUrl = runtime.testMySqlUrl;
+            const { result } = await runtime.Sql.query({ connectionUrl, query: 'SELECT * FROM test;', params: [] });
+            assert.ok(result);
+        });
+    });
 
-// });
+});
